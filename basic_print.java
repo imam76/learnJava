7aa63862b3383025a8fe1c5041d0b575b76c38c3
Imam Nawawi <asepimam335@gmail.com>
2018-05-21 11:15:41 +0700

    second commit
    perbedaan print dan println

---
+++ basic_print.java
@@ -0,0 +1,12 @@
+/*Asep Imam Nawawi*/
+class Main {
+	public static void main(String[] foo){
+      	//println sifatnya itu new line
+		System.out.println("Data");
+      	System.out.println("data Imam");
+      	System.out.println("-------------------");
+      	//sedang kan print sifatnya same line
+      	System.out.print("imam nawawi ");
+      	System.out.print("+ imam nawawi");
+	}
+}
